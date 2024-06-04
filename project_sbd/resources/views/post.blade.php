81b9f19b6f4dc4e04016048905ba7b66da1e0b56
Drakenvei <nicholaskensurya@gmail.com>
2024-06-04 13:59:08 +0700

    Pasang Template

--- project_sbd/resources/views/post.blade.php
+++ project_sbd/resources/views/post.blade.php
@@ -1,105 +1,99 @@
-<!DOCTYPE html>
-<html>
+@extends('layout')
 
-<head>
-    <title>Post</title>
-    <link rel="stylesheet" href="{{ asset('css/post.css') }}">
-</head>
+@section('title', 'Dashboard')
 
-<body>
-    <div class="main">
-        <h1>Post</h1>
-        <form action="{{ route('posts.search') }}" method="GET">
-            <div>
-                <select name="category">
-                    <option value="">Select Category</option>
-                    @foreach ($categories as $category)
-                        <option value="{{ $category->id }}" {{ request('category') == $category->id ? 'selected' : '' }}>
-                            {{ $category->name }}
-                        </option>
-                    @endforeach
-                </select>
-                
-                <select name="tag">
-                    <option value="">Select Tag</option>
-                    @foreach ($tags as $tag)
-                        <option value="{{ $tag->id }}" {{ request('tag') == $tag->id ? 'selected' : '' }}>
-                            {{ $tag->name }}
-                        </option>
-                    @endforeach
-                </select>
-                
-                <input class="search" type="text" name="title" placeholder="Search by title..." value="{{ request('title') }}">
-                <button class="hidden xl:inline-flex items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-1 sm:ml-3" type="submit">Search</button>
-            </div>
-        </form>
-        <table border="solid">
-            <thead>
-                <tr>
-                    <th>
-                        <a href="{{ route('posts.search', array_merge(request()->all(), ['sort' => 'id', 'direction' => request('direction') == 'asc' ? 'desc' : 'asc'])) }}">
-                            ID
-                            @if (request('sort') == 'id')
-                                @if (request('direction') == 'asc')
-                                    &#9650; <!-- Up arrow -->
-                                @else
-                                    &#9660; <!-- Down arrow -->
-                                @endif
-                            @endif
-                        </a>
-                    </th>
-                    <th>Title</th>
-                    <th style="width: 250px">Content</th>
-                    <th>Slug</th>
-                    <th>
-                        <a href="{{ route('posts.search', array_merge(request()->all(), ['sort' => 'date_updated', 'direction' => request('direction') == 'asc' ? 'desc' : 'asc'])) }}">
-                            Date Updated
-                            @if (request('sort') == 'date_updated')
-                                @if (request('direction') == 'asc')
-                                    &#9650; <!-- Up arrow -->
-                                @else
-                                    &#9660; <!-- Down arrow -->
-                                @endif
-                            @endif
-                        </a>
-                    </th>
-                    <th>Category</th>
-                    <th>Tags</th>
-                    <th>Author</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($posts as $post)
-                    <tr>
-                        <td>{{ $post->id }}</td>
-                        <td>{{ $post->title }}</td>
-                        <td class="img text-center"><img class="image" style="width: 250px" src="{{ $post->image }}" alt="image"></td>
-                        <td>{{ $post->slug }}</td>
-                        <td>{{ $post->date_updated }}</td>
-                        <td>
-                            <ul>
-                                @foreach ($post->categories as $category)
-                                    <li>{{ $category->name }}</li>
-                                @endforeach
-                            </ul>
-                        </td>
-                        <td>
-                            <ul>
-                                @foreach ($post->tags as $tag)
-                                    <li>{{ $tag->name }}</li>
-                                @endforeach
-                            </ul>
-                        </td>
-                        <td>{{ $post->author->name }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+@section('page-title', 'Dashboard')
 
-        <div class="pagination">
-            {{ $posts->links() }}
-        </div>
+@section('content')
+<h1>Post</h1>
+<form action="{{ route('posts.search') }}" method="GET">
+    <div>
+        <select name="category">
+            <option value="">Select Category</option>
+            @foreach ($categories as $category)
+                <option value="{{ $category->id }}" {{ request('category') == $category->id ? 'selected' : '' }}>
+                    {{ $category->name }}
+                </option>
+            @endforeach
+        </select>
+        
+        <select name="tag">
+            <option value="">Select Tag</option>
+            @foreach ($tags as $tag)
+                <option value="{{ $tag->id }}" {{ request('tag') == $tag->id ? 'selected' : '' }}>
+                    {{ $tag->name }}
+                </option>
+            @endforeach
+        </select>
+        
+        <input class="search" type="text" name="title" placeholder="Search by title..." value="{{ request('title') }}">
+        <button class="hidden xl:inline-flex items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-1 sm:ml-3" type="submit">Search</button>
     </div>
-</body>
+</form>
+<table border="solid">
+    <thead>
+        <tr>
+            <th>
+                <a href="{{ route('posts.search', array_merge(request()->all(), ['sort' => 'id', 'direction' => request('direction') == 'asc' ? 'desc' : 'asc'])) }}">
+                    ID
+                    @if (request('sort') == 'id')
+                        @if (request('direction') == 'asc')
+                            &#9650; <!-- Up arrow -->
+                        @else
+                            &#9660; <!-- Down arrow -->
+                        @endif
+                    @endif
+                </a>
+            </th>
+            <th>Title</th>
+            <th style="width: 250px">Content</th>
+            <th>Slug</th>
+            <th>
+                <a href="{{ route('posts.search', array_merge(request()->all(), ['sort' => 'date_updated', 'direction' => request('direction') == 'asc' ? 'desc' : 'asc'])) }}">
+                    Date Updated
+                    @if (request('sort') == 'date_updated')
+                        @if (request('direction') == 'asc')
+                            &#9650; <!-- Up arrow -->
+                        @else
+                            &#9660; <!-- Down arrow -->
+                        @endif
+                    @endif
+                </a>
+            </th>
+            <th>Category</th>
+            <th>Tags</th>
+            <th>Author</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($posts as $post)
+            <tr>
+                <td>{{ $post->id }}</td>
+                <td>{{ $post->title }}</td>
+                <td class="img text-center"><img class="image" style="width: 250px" src="{{ $post->image }}" alt="image"></td>
+                <td>{{ $post->slug }}</td>
+                <td>{{ $post->date_updated }}</td>
+                <td>
+                    <ul>
+                        @foreach ($post->categories as $category)
+                            <li>{{ $category->name }}</li>
+                        @endforeach
+                    </ul>
+                </td>
+                <td>
+                    <ul>
+                        @foreach ($post->tags as $tag)
+                            <li>{{ $tag->name }}</li>
+                        @endforeach
+                    </ul>
+                </td>
+                <td>{{ $post->author->name }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+    
+</table>
+       
+            {{ $posts->links() }}
 
-</html>
+@endsection
